2cd3eeffc76bf3debaf04269712ab689b7fc6012
Simon Peckmore <simon.peckmore@mail.nidec.com>
2025-04-25 13:13:30 +0100

    First version with support for `MainMenu` and `ContextMenu`.

---
+++ src/System.Windows.Forms.Legacy/Shims/Command.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using System.Windows.Forms;
+
+namespace System.Windows
+{
+    /// <summary>
+    /// An implementation of `Command` that wraps the internal WinForms implementation. We have to use the built-in implementation as
+    /// it is used to dispatch `WM_COMMAND` messages.
+    /// 
+    /// Credit to Inmobilis (https://github.com/Inmobilis) for this solution.
+    /// https://github.com/paintdotnet/System.Windows.Forms.Legacy/issues/3#issuecomment-1463645942
+    /// </summary>
+    internal class Command : IDisposable
+    {
+        #region Fields
+
+        #region Private
+
+        private readonly object _cmd;
+
+        #endregion
+
+        #region Private Static
+
+        private static readonly Type CommandType = typeof(Control).Assembly.GetType("System.Windows.Forms.Command")!;
+        private static readonly PropertyInfo CommandIdProperty = CommandType.GetProperty(nameof(ID), typeof(int))!;
+        private static readonly MethodInfo CommandDisposeMethod = CommandType.GetMethod(nameof(Dispose), Type.EmptyTypes)!;
+
+        #endregion
+
+        #endregion
+
+        #region Construction
+
+        public Command(ICommandExecutor target)
+        {
+            _cmd = Activator.CreateInstance(CommandType, target) ?? throw new NullReferenceException("'target' parameter cannot be null.");
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int ID => (int)CommandIdProperty.GetValue(_cmd)!;
+
+        #endregion
+
+        #region Methods
+
+        public void Dispose()
+        {
+            CommandDisposeMethod.Invoke(_cmd, null);
+        }
+
+        #endregion
+    }
+}
---
+++ src/System.Windows.Forms.Legacy/Shims/SRShim.cs
@@ -0,0 +1,51 @@
+﻿using System.Resources;
+
+namespace System
+{
+    /// <summary>
+    /// A resource file shim to encapsulate functionality from the `SR` class in `System.Drawing.Common\Special`. This functionality was
+    /// implemented as a shim to prevent a name collision with the original methods in WinForms, but the bulk of the code is taken from
+    /// the original implementation in the WinForms repo.
+    /// </summary>
+    internal static class SRShim
+    {
+        #region Fields
+
+        private static readonly bool s_usingResourceKeys = AppContext.TryGetSwitch("System.Resources.UseSystemResourceKeys", out bool usingResourceKeys)
+                                                           && usingResourceKeys;
+
+        #endregion
+
+        #region Methods
+
+        // This method is used to decide if we need to append the exception message parameters to the message when calling SR.Format.
+        // by default it returns the value of System.Resources.UseSystemResourceKeys AppContext switch or false if not specified.
+        // Native code generators can replace the value this returns based on user input at the time of native code generation.
+        // The Linker is also capable of replacing the value of this method when the application is being trimmed.
+        internal static bool UsingResourceKeys()
+        {
+            return s_usingResourceKeys;
+        }
+        internal static string GetResourceString(string resourceKey)
+        {
+            if (UsingResourceKeys())
+            {
+                return resourceKey;
+            }
+
+            string? resourceString = null;
+            try
+            {
+                resourceString = ""; //ResourceManager.GetString(resourceKey);
+            }
+            catch (MissingManifestResourceException)
+            {
+                // Swallow exception.
+            }
+
+            return resourceString!; // only null if missing resources
+        }
+
+        #endregion
+    }
+}
